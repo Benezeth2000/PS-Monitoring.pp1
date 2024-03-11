db7fa74381b19ef20daec0a50d76c29ba250f56e
Benezeth2000 <sengekabenh@gmail.com>
2024-03-11 16:38:19 +0300

    Initial commit

---
+++ app/src/main/java/com/visthome/psmonitoringapp/MedicalReport.java
@@ -0,0 +1,14 @@
+package com.visthome.psmonitoringapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MedicalReport extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medical_report);
+    }
+}
